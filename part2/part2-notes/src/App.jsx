86dea3a0dc97c4209a80b9584fd14b9e3e23c9f3
Kafilat <kafilatadewumi61@gmail.com>
2023-02-18 23:26:34 +0100

    update:part2-note

--- part2/part2-notes/src/App.jsx
+++ part2/part2-notes/src/App.jsx
@@ -1,11 +1,18 @@
-import './App.css';
+import React from 'react'
+import Note from './Components/Note'
 
-function App() {
+const App = ({notes}) => {
   return (
-    <div className="App">
-      
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {
+          notes.map(note => 
+          <Note key={note.id} note={note}/>)
+        }
+      </ul>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
